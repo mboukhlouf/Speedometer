9682e819c884cfc46a88add23b1a56bdb98f1fb0
anoncrash <awards@gmail.com>
2019-01-23 19:59:54 +0100

    Initial commit

--- Speedometer/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Speedometer
-{
-    public class Class1
-    {
-    }
-}
---
+++ Speedometer/Speedometer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Speedometer
+{
+    public class Speedometer
+    {
+        private int StartTickCount;
+        public int Progress { get; set; }
+
+        public void Start()
+        {
+            Progress = 0;
+            StartTickCount = Environment.TickCount;
+        }
+
+        public double GetProgressPerSecond()
+        {
+            return Progress / TimeSpan.FromMilliseconds(Environment.TickCount - StartTickCount).TotalSeconds;
+        }
+
+        public double GetProgressPerHour()
+        {
+            return Progress / TimeSpan.FromMilliseconds(Environment.TickCount - StartTickCount).TotalHours;
+        }
+    }
+
+}
